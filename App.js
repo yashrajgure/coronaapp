aa8a86d5601a6a674bf93cfda53c4b98413f271e
yashrajgure <yashrajgure0808@gmail.com>
2020-04-04 18:33:17 +0530

    Update App.js and add images in assets

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ImageBackground, Dimensions, TouchableWithoutFeedback, Keyboard, TouchableHighlight } from 'react-native';
 import axios from 'axios';
 export default function App() {
 
@@ -7,10 +7,11 @@ export default function App() {
 
   const [country,setCountry]= useState('')
   
-  const [newCountry,setNewCountry] = useState('USA')
+  const [newCountry,setNewCountry] = useState('world')
 
   const buttonHandler = () => {
-    setNewCountry(country)
+    setNewCountry(country);
+    Keyboard.dismiss();
   }
 
   useEffect( () => {
@@ -26,15 +27,35 @@ export default function App() {
   }, [newCountry])
 
   return (
-    <View style={styles.container}>
-      <Text>Enter Country:</Text>
-      <TextInput 
-        placeholder= 'default: USA'
-        style={styles.input}
-        onChange= { val => setCountry(val)}/>
-      <Button onPress={() => buttonHandler() } title='Check'/>
-      <Text>The Cases are: { info.cases }</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={()=> {
+      Keyboard.dismiss();
+    }}>
+      <ImageBackground source={require('./assets/a1.png')} style={styles.container}>
+        <TextInput 
+          placeholder= 'Enter Country'
+          style={styles.input}
+          onChangeText= { val => setCountry(val)}/>
+        <TouchableHighlight 
+          style={styles.button} 
+          underlayColor= 'coral'
+          activeOpacity={0.7}
+          onPress={ () => buttonHandler() }>
+          <Text style={styles.buttonText}>CHECK</Text>
+        </TouchableHighlight>
+        <View style={styles.output}>
+          <Text style={styles.head}>Country:</Text>
+          <Text style={styles.text}>{ info.country }</Text>
+          <Text style={styles.head}>Total Cases:</Text>
+          <Text style={styles.text}>{ info.cases }</Text>
+          <Text style={styles.head}>Active Cases:</Text>
+          <Text style={styles.text}>{ info.active }</Text>
+          <Text style={styles.head}>Deaths:</Text>
+          <Text style={styles.text}>{ info.deaths }</Text>
+          <Text style={styles.head}>Recovered:</Text>
+          <Text style={styles.text}>{ info.recovered }</Text>
+        </View>
+      </ImageBackground>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -44,14 +65,63 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 100,
+    position: 'absolute',
+    left:0,
+    top:0,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height+100,
   },
   input:{
-    borderWidth: 1,
-    borderColor: '#777',
+    borderWidth: 3,
+    borderRadius: 50,
+    borderColor: 'white',
     padding: 10,
-    margin: 10,
-    width: 200,
+    margin: 20,
+    width: 300,
+    height: 60,
     textAlign: 'center',
-}
+    color:'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    backgroundColor: '#3A3838',
+  },
+  output:{
+    marginTop: 40,
+    padding : 30,
+    fontSize: 20,
+    backgroundColor: '#3A3838',
+    borderRadius: 30,
+  },
+  head:{
+    fontSize:24,
+    alignSelf:'center',
+    fontWeight:'bold',
+    color: 'white',
+  },
+  text:{
+    fontSize:20,
+    alignSelf:'center',
+    fontWeight:'bold',
+    color: 'coral',
+  },
+  button:{
+    borderWidth: 3,
+    borderRadius: 50,
+    borderColor: 'white',
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 15,
+    width: 150,
+    height: 50,
+    backgroundColor: 'white',
+    shadowColor: '#3A3838',
+  },
+  buttonText:{
+    fontSize:20,
+    alignSelf:'center',
+    fontWeight:'bold',
+    justifyContent:"center",
+  },
 });
